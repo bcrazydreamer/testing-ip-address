e86ed394f71283646d192414337d4beb9262cf92
Bharat Rawat <31788510+bcrazydreamer@users.noreply.github.com>
2022-09-27 01:32:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,24 @@
-var express = require('express');
-var app = express();
-
-require('dotenv').config()
+const express = require('express');
+const requestIp = require('request-ip');
+const app = express();
 
 app.set('port', (process.env.PORT || 5000));
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/public');
 
+app.use(requestIp.mw())
+app.use(function(req, res, next) {
+  const ip = req.clientIp;
+  try{
+    console.log("Client IP-1:", req.socket.remoteAddress);
+  } catch(err){}
+  console.log("Client IP-2:", ip);
+  next();
+});
+
 app.get('/', function(request, response) {
-  var env = process.env.APP_ENV;
-  if (env == 'staging') {
-    var envName = 'staging'
-  } else if (env == 'production') {
-    var envName = 'production'
-  } else {
-    var envName = 'review app'
-  }
-  response.render('index.html', { env: envName});
+  response.render('index.html');
 });
 
 app.listen(app.get('port'), function() {
